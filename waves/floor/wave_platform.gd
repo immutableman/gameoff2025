0c8053accf6e849d8db8be7e9bf15ea42ed21101
Neal <immutableman@users.noreply.github.com>
2025-11-03 10:43:10 -0800

    amplitude curves

--- waves/floor/wave_platform.gd
+++ waves/floor/wave_platform.gd
@@ -3,6 +3,9 @@ extends Node2D
 @export var speed: float = 300
 @export var wavelength: float = 200
 @export var amplitude: float = 40
+@export var amplitude_curve: Curve
+
+## Offset into the sine wave to start at. Set non-zero to line up with other platforms
 @export var offset: float = 0
 
 var line: Path2D
@@ -15,19 +18,24 @@ func _ready() -> void:
 		if child is Path2D:
 			line = child
 
-	var offset = 0
+	var length = line.curve.get_baked_length()
+
 	var points = line.curve.get_baked_points()
 	for i in range(points.size() - 1):
 		var p0 = points[i]
 		var p1 = points[i + 1]
-		var pMid = p0.lerp(p1, 0.5)
-		var angle = p0.angle_to_point(p1)
+		var pMid = p0.lerp(p1, 0.5)  # create the node at the midpoint
+		var angle = p0.angle_to_point(p1)  # node should face the next point
 		var node = WaveNodes.alloc_node()
 		node.position = pMid
 		node.rotation = angle
 		node.speed = speed
 		node.wavelength = wavelength
-		node.amplitude = amplitude
-		node.offset = p0.distance_to(pMid) + offset
+		var t = offset / length
+		if amplitude_curve:
+			node.amplitude = amplitude * amplitude_curve.sample(t)
+		else:
+			node.amplitude = amplitude
+		node.offset = p0.distance_to(pMid) + offset  # distance along the wave
 		add_child(node)
-		offset += p0.distance_to(p1)
+		offset += p0.distance_to(p1)  # update the cumulative offset
