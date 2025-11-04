afd8164bf5a3154a30fec8ef878c323cf1ff4830
Neal <immutableman@users.noreply.github.com>
2025-11-03 18:55:00 -0800

    rerender spikes dynamically

--- waves/floor/spikes.gd
+++ waves/floor/spikes.gd
@@ -1,36 +1,38 @@
 @tool
 extends Node2D
 
+@export var spike_scene: PackedScene = preload("res://floor/spike.tscn")
+
 var _line: Path2D
 var _spikes: Array = []
-
-func _enter_tree() -> void:
-	if Engine.is_editor_hint():
-		_render_line()
+var _cached_points: PackedVector2Array
 
 func _ready() -> void:
+	_find_line()
 	if not Engine.is_editor_hint():
 		_render_line()
 
-func _render_line():
-	# TODO cache better
-	for spike in _spikes:
-		spike.queue_free()
-	_spikes.clear()
-	
+func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		if _cached_points != _line.curve.get_baked_points():
+			for spike in _spikes:
+				spike.queue_free()
+			_spikes.clear()
+			_render_line()
+
+func _find_line():
 	for child in get_children():
 		if child is Path2D:
 			_line = child
 			_line.curve.bake_interval = 64
 
-	var node_scene = load("res://floor/spike.tscn")
-
-	var points = _line.curve.get_baked_points()
-	for i in range(points.size() - 1):
-		var p0 = points[i]
-		var p1 = points[i + 1]
+func _render_line():
+	_cached_points = _line.curve.get_baked_points()
+	for i in range(_cached_points.size() - 1):
+		var p0 = _cached_points[i]
+		var p1 = _cached_points[i + 1]
 		var pMid = p0.lerp(p1, 0.5)  # create the node at the midpoint
-		var spike = node_scene.instantiate()  # TEMP
+		var spike = spike_scene.instantiate()
 		spike.position = pMid
 		add_child(spike)
 		_spikes.push_back(spike)
