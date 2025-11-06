c7ad9aaa96d85d06cb0a17bf083b30bb3af6daa7
Neal <immutableman@users.noreply.github.com>
2025-11-06 14:05:41 -0800

    levels

--- waves/main_menu/level_button.gd
+++ waves/main_menu/level_button.gd
@@ -5,3 +5,7 @@ var _data: LevelData
 func set_level_data(data: LevelData) -> void:
 	_data = data
 	$Button.text = data.display_name
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_packed(_data.scene)
--- waves/main_menu/main_menu.gd
+++ waves/main_menu/main_menu.gd
@@ -1,11 +1,14 @@
-extends Control
+extends CanvasLayer
 
 @export var level_list: Array[LevelData]
 
-
+const level_button_scene = preload("res://main_menu/level_button.tscn")
 
 func _ready() -> void:
 	for node in $%Levels.get_children():
 		node.queue_free()
+		
 	for level in level_list:
-		pass
+		var btn = level_button_scene.instantiate()
+		btn.set_level_data(level)
+		$%Levels.add_child(btn)
