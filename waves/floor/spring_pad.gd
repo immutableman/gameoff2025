f3a7f3b84db298003e6db68370b87f8106300099
Neal <immutableman@users.noreply.github.com>
2025-11-28 17:21:43 -0800

    series of tubes

--- waves/floor/spring_pad.gd
+++ waves/floor/spring_pad.gd
@@ -3,6 +3,8 @@ extends Control
 @export var speed: float = 1500
 @export var seconds_to_max_speed: float = 0.1
 
+var initial_velocity = Vector2()
+
 func _ready() -> void:
 	$Area2D/CollisionShape2D.shape.size = size
 	
@@ -15,13 +17,21 @@ func _ready() -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	initial_velocity = Vector2.ZERO
 	body.owner.add_mover(self)
 	$LockoutTimer.start()
 	await $LockoutTimer.timeout
 	body.owner.remove_mover(self)
+	initial_velocity = Vector2.ZERO
 
 func calc_mover_velocity(velocity: Vector2, delta: float) -> Vector2:
-	var target_velocity = speed * Vector2.UP.rotated(rotation)
+	if initial_velocity.is_zero_approx():
+		var dir = Vector2.UP.rotated(rotation)
+		var projection_a_on_b = dir * velocity.dot(dir)
+		var orthogonal_component = velocity - projection_a_on_b
+		initial_velocity = orthogonal_component
+	var target_velocity = initial_velocity + speed * Vector2.UP.rotated(rotation)
+	#var target_velocity = speed * Vector2.UP.rotated(rotation)
 	var elapsed = $LockoutTimer.wait_time - $LockoutTimer.time_left
 	var t = clamp(elapsed / seconds_to_max_speed, 0, 1)
 	return velocity.lerp(target_velocity, t)
