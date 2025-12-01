7b826540c192989a3007982a6a776264d8b454a4
Neal <immutableman@users.noreply.github.com>
2025-12-01 12:30:25 -0800

    perf experiment

--- waves/floor/wave_node.gd
+++ waves/floor/wave_node.gd
@@ -5,16 +5,13 @@ extends AnimatableBody2D
 @export var amplitude: float = 40
 @export var offset: float = 0
 
-var _elapsed: float = 0
 var _start_position: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_start_position = position
-	#print(wave_delta)
 
 func _physics_process(delta: float) -> void:
-	_elapsed += delta
 	var up = transform.basis_xform(Vector2.UP)
-	var wave_delta = amplitude * sin((offset - _elapsed * speed) * PI / wavelength)
+	var wave_delta = amplitude * sin((offset - WaveNodes.elapsed * speed) * PI / wavelength)
 	position = _start_position + up * wave_delta
--- waves/floor/wave_nodes.gd
+++ waves/floor/wave_nodes.gd
@@ -4,6 +4,11 @@ extends Node
 
 @onready var node_scene: PackedScene = preload("res://floor/wave_node.tscn")
 
+var elapsed: float = 0
+
+func _physics_process(delta: float) -> void:
+	elapsed += delta
+
 func alloc_node() -> WaveNode:
 	return node_scene.instantiate()
 
